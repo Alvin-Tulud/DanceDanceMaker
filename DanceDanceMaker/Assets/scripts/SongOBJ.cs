33838469693185509cde9d8ae0aeec3308021954
Alvin-Tulud <alvintulud@gmail.com>
2025-01-24 02:57:40 -0800

    start scriptable obj

---
+++ DanceDanceMaker/Assets/scripts/SongOBJ.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "SongOBJ", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+public class SongOBJ : ScriptableObject
+{
+    public AudioClip song;
+
+}
